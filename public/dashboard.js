5799624be48c0e0fd3110407fd435f9582c48d2e
MeghanaGanesuni10 <meghanaganesuni@gmail.com>
2026-06-17 20:05:54 +0530

    Update dashboard.js

--- public/dashboard.js
+++ public/dashboard.js
@@ -11,16 +11,3 @@ fetch('/api/user')
         window.location.href = "login.html";
     }
 });
-fetch('/api/user')
-.then(res => res.json())
-.then(data => {
-    if (data.success) {
-        document.getElementById("welcome").innerText =
-            "Welcome, " + data.user.name + " 🎉";
-
-        document.getElementById("role").innerText =
-            "Role: " + data.user.role.toUpperCase();
-    } else {
-        window.location.href = "login.html";
-    }
-});
